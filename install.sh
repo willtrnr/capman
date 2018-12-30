38e43df9291b6dff7559861059931c735911adc3
William Turner <willtur.will@gmail.com>
2018-12-30 18:02:43 -0500

    Improve install script instructions and log

--- install.sh
+++ install.sh
@@ -9,41 +9,49 @@ ARCH='x86_64'
 REPO="https://storage.googleapis.com/capman-repo/core/os/$ARCH"
 PKGEXT='.pkg.tar.xz'
 
-SUDO="/usr/bin/sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+PACMAN_VER='5.1.2-1'
+KEYRING_VER='20181230-1'
 
-echo 'Installing runtime dependencies with crew...'
-for pkg in glibc curl gpgme xzutils libarchive; do
-  yes | crew install $pkg
-done
+SUDO="/usr/bin/sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
 install_pkg() {
   curl -Lo "${TMPDIR}/${1}${PKGEXT}" "${REPO}/${1}${PKGEXT}"
   (cd /; $SUDO tar --warning=none -xf "${TMPDIR}/${1}${PKGEXT}" usr/local/)
 }
 
-echo 'Installing pacman...'
-install_pkg "pacman-5.1.2-1-$ARCH"
+echo '==> Installing runtime dependencies with crew...'
+for pkg in glibc curl gpgme xzutils libarchive; do
+  yes | crew install $pkg
+done
 
-echo 'Installing the keyring...'
-install_pkg 'capman-keyring-20181230-1-any'
+echo -e "\e[33m"
+echo 'From here on things will mostly be done as `root`, enter'
+echo 'your `sudo` password if prompted.'
+echo -e "\e[0m"
 
-echo 'Initializing the keyring...'
+echo '==> Installing pacman...'
+install_pkg "pacman-$PACMAN_VER-$ARCH"
+
+echo '==> Installing the keyring...'
+install_pkg "capman-keyring-$KEYRING_VER-any"
+
+echo '==> Initializing the keyring...'
 $SUDO /usr/local/bin/pacman-key --init
 $SUDO /usr/local/bin/pacman-key --populate capman
 
-echo 'Syncing repositories...'
+echo '==> Syncing repositories...'
 $SUDO /usr/local/bin/pacman -Sy
 
-echo 'Taking ownership of the package files...'
+echo '==> Taking ownership of the package files...'
 $SUDO /usr/local/bin/pacman -Sdd --noconfirm --overwrite '/usr/local/*' pacman capman-keyring glibc curl gpgme libarchive
 
-echo 'Building the local bash package...'
+echo '==> Building the local bash wrapper...'
 mkdir -p "$TMPDIR/bash"
 curl -Lo "$TMPDIR/bash/PKGBUILD" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/bash/PKGBUILD
 (cd "$TMPDIR/bash"; makepkg -d)
 $SUDO /usr/local/bin/pacman -Udd --noconfirm "$TMPDIR/bash/bash-"*"$PKGEXT"
 
-echo 'Building the local sudo package...'
+echo '==> Building the local sudo wrapper...'
 mkdir -p "$TMPDIR/sudo"
 curl -Lo "$TMPDIR/sudo/PKGBUILD" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/sudo/PKGBUILD
 curl -Lo "$TMPDIR/sudo/sudo.sh" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/sudo/sudo.sh
@@ -52,5 +60,6 @@ $SUDO /usr/local/bin/pacman -Udd --noconfirm "$TMPDIR/sudo/sudo-"*"$PKGEXT"
 
 echo -e "\e[32m"
 echo 'All done! At this point you must no longer use `crew` to install packages.'
-echo 'Instead use `pacman -S <name>` to install and `pacman -Syu` to update.'
+echo 'Instead use `sudo pacman -S <name>` to install and `sudo pacman -Syu`'
+echo 'to update your installed packages.'
 echo -e "\e[0m"
