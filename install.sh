46d03d393926fd8e8511f7d9f382f01153269163
William Turner <willtur.will@gmail.com>
2018-12-31 18:38:13 -0500

    Add local linux headers building to install script

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-TMPDIR="$(mktemp -d)"
-trap "rm -rf '${TMPDIR}'" EXIT
+TMPDIR="/usr/local/tmp/capman"
+mkdir -p "$TMPDIR"
+trap "rm -rf '$TMPDIR'" EXIT
 
 ARCH='x86_64'
 REPO="https://storage.googleapis.com/capman-repo/core/os/$ARCH"
@@ -13,6 +14,7 @@ PACMAN_VER='5.1.2-1'
 KEYRING_VER='20181230-1'
 
 SUDO="/usr/bin/sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+PACMAN="$SUDO /usr/local/bin/pacman --noconfirm"
 
 install_pkg() {
   curl -Lo "${TMPDIR}/${1}${PKGEXT}" "${REPO}/${1}${PKGEXT}"
@@ -40,23 +42,36 @@ $SUDO /usr/local/bin/pacman-key --init
 $SUDO /usr/local/bin/pacman-key --populate capman
 
 echo '==> Syncing repositories...'
-$SUDO /usr/local/bin/pacman -Sy
+$PACMAN -Sy
 
-echo '==> Taking ownership of the package files...'
-$SUDO /usr/local/bin/pacman -Sdd --noconfirm --overwrite '/usr/local/*' pacman capman-keyring glibc curl gpgme libarchive
+echo '==> Installing base packages and taking ownership of files...'
+$PACMAN -Sdd --noconfirm --overwrite '/*' \
+  filesystem \
+  glibc \
+  curl \
+  gpgme \
+  libarchive \
+  pacman \
+  capman-keyring
 
-echo '==> Building the local bash wrapper...'
+echo '==> Building the local Linux headers package...'
+mkdir -p "$TMPDIR/linux-api-headers"
+curl -Lo "$TMPDIR/linux-api-headers/PKGBUILD" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/linux-api-headers/PKGBUILD
+(cd "$TMPDIR/linux-api-headers"; makepkg -d)
+$PACMAN -Udd --noconfirm --overwrite '/*' "$TMPDIR/linux-api-headers/linux-api-headers-"*"$PKGEXT"
+
+echo '==> Building the local bash wrapper package...'
 mkdir -p "$TMPDIR/bash"
 curl -Lo "$TMPDIR/bash/PKGBUILD" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/bash/PKGBUILD
 (cd "$TMPDIR/bash"; makepkg -d)
-$SUDO /usr/local/bin/pacman -Udd --noconfirm "$TMPDIR/bash/bash-"*"$PKGEXT"
+$PACMAN -Udd --noconfirm --overwrite '/*' "$TMPDIR/bash/bash-"*"$PKGEXT"
 
-echo '==> Building the local sudo wrapper...'
+echo '==> Building the local sudo wrapper package...'
 mkdir -p "$TMPDIR/sudo"
 curl -Lo "$TMPDIR/sudo/PKGBUILD" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/sudo/PKGBUILD
 curl -Lo "$TMPDIR/sudo/sudo.sh" https://raw.githubusercontent.com/wwwiiilll/capman/master/local/sudo/sudo.sh
 (cd "$TMPDIR/sudo"; makepkg -d)
-$SUDO /usr/local/bin/pacman -Udd --noconfirm "$TMPDIR/sudo/sudo-"*"$PKGEXT"
+$PACMAN -Udd --noconfirm --overwrite '/*' "$TMPDIR/sudo/sudo-"*"$PKGEXT"
 
 echo -e "\e[32m"
 echo 'All done! At this point you must no longer use `crew` to install packages.'
